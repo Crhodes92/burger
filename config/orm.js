f7ca8d1e649598e159bb7941d12e9e4c84769ffd
Conner Rhodes <crhodes2146@gmail.com>
2018-11-28 21:27:18 -0600

    created db, config folder, and models file with burger.js. Needs controller next.

---
+++ config/orm.js
@@ -0,0 +1,78 @@
+var connection = require("connection.js");
+
+
+//prints question marks to an array to help generate MYSQL syntax
+function printsQMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+//helps create mysql syntax from the key/value pairs
+function objToSql(obj) {
+    var arr = [];
+
+    for (var key in obj) {
+        arr.push(key + "=" + obj[key]);
+    }
+
+    return arr.toString();
+}
+
+//creates the orm object to make queries to the database
+var orm = {
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    //method of the orm that adds an entry to the db
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+      updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+    };
+
+module.exports = orm;
+    
