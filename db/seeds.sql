d86fdae3e79d8217821137252dfea269b44ab36a
Conner Rhodes <crhodes2146@gmail.com>
2018-11-28 20:41:58 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Feelin Blue Cheese Burger", 0);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Gourdon-Hamsey Burger", 0);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Poutine on the Ritz Burger", 0);
